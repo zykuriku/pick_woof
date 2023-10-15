bfe6739c19b6dcd8d11ec5381f764c6fda8ade27
zykuriku <104116273+zykuriku@users.noreply.github.com>
2023-10-15 17:06:02 +0530

    inital commit

---
+++ android/app/src/main/kotlin/com/example/pick_woof/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pick_woof
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PickWoof());
+
+class PickWoof extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
